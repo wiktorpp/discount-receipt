6ba718512661bcfd1c84b5cf3d261f97a3a5278e
wiktorpp <56189344+wiktorpp@users.noreply.github.com>
2023-02-25 15:47:53 +0100

    Added printing over network using RawBT

--- discount-receipt.py
+++ discount-receipt.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from os import mkdir, getcwd
+from os import mkdir, getcwd, listdir
+import socket
 
 class Product:
     def __init__(
@@ -32,8 +33,6 @@ code_to_product = {
 receipt_header = "Zloty kolczyk\n<ulica>\nTel. <numer>\n--------------------------------\n"
 receipt_footer = "--------------------------------\n      Zapraszamy ponownie\n"
 
-
-
 def generate_receipt(products, date="", preview=False):
     receipt=receipt_header
     for index, product in enumerate(purchased_products):
@@ -48,25 +47,29 @@ def generate_receipt(products, date="", preview=False):
             difference = str(product.final_price - product.starting_price)
             difference = f"{difference[:-2]}.{difference[-2:]}"
         final_price = f"{str(product.final_price)[:-2]}.{str(product.final_price)[-2:]}"
-        receipt += f"{product.name:<19}{'-' + discount_percentage:>4}%\n"
-        receipt += f"         {starting_price:>7} {difference:>7} {final_price:>7}\n"
+        receipt += f"{product.name:<19}{'~' + discount_percentage:>4}%\n"
+        receipt += f"         {starting_price:>7} {difference:>7} {'~' + final_price:>7}\n"
     receipt += receipt_footer
     return receipt
 
-def print_receipt(receipt, date):
-    filename = date.replace(" ", "_").replace(":", "+") + ".txt"
+def save_receipt_as_txt(receipt, filename):
     try:
-        mkdir(f"{getcwd()}/receipts/")
+        mkdir(f"{getcwd()}/printed/")
     except:
         pass
-    with open(f"{getcwd()}/receipts/{filename}", "w+") as file:
+    with open(f"{getcwd()}/printed/{filename}", "w+") as file:
         file.write(receipt)
+        
+def print_receipt_network_printer(receipt):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("localhost",9100))
+    s.send(receipt.encode())
+    s.close()
 
-def save_logs(products, date):
+def save_logs(products, filename):
     out = ""
     for product in products:
         out += product.to_csv() + "\n"
-    filename = date.replace(" ", "_").replace(":", "+") + ".csv"
     try:
         mkdir(f"{getcwd()}/logs/")
     except:
@@ -77,16 +80,18 @@ def save_logs(products, date):
 purchased_products = []
 while True:
     print(
-        "1. Add product - 1\n"
-        "2. Remove product - 2\n"
-        "3. Clear - 3\n"
-        "4. Save and print - 4"
+        "1. Add product\n"
+        "2. Remove product\n"
+        "3. Clear\n"
+        "4. Save and print\n"
+        "5. Print file"
     )
     # print(
     #     "1. Dodaj produkt\n"
     #     "2. Usuń produkt\n"
     #     "3. Wyczyść\n"
-    #     "4. Zapisz i drukuj"
+    #     "4. Zapisz i drukuj\n"
+    #     "5. Drukuj plik\n"
     # )
     option = input("#> ")
     if option == "1":
@@ -102,10 +107,12 @@ while True:
                 discount_percentage = 0
                 if len(name) == 0:
                     print("Can't be empty")
+                    # print("Nie może być puste")
                 else:
                     break
             except KeyError:
                 print("Invalid code")
+                # print("Niepoprawny kod")
             else:
                 name = product.name
                 discount_percentage = product.discount_percentage
@@ -121,16 +128,19 @@ while True:
                     starting_price = int(f"{starting_price}00")
             except ValueError:
                 print("Invalid price")
+                # print("Niepoprawna cena")
             else:
                 break
         while True:
             print("Enter discount")
+            # Print("Wpisz zniszkę")
             value = input(f"{discount_percentage}%> ")
             if value != "":
                 try:
                     discount_percentage = int(value)
                 except ValueError:
                     print("Invalid value")
+                    # print("Niepoprawna wartość")
                 else:
                     break
             else:
@@ -147,36 +157,72 @@ while True:
                         final_price = int(f"{value}00")
                 except ValueError:
                     print("Invalid value")
+                    # print("Niepoprawna wartość")
                 else:
                     break
             else:
                 break
         purchased_products.append(Product(name, discount_percentage, starting_price, final_price))
+
     elif option == "2":
         print(generate_receipt(purchased_products, preview=True))
         try:
             index=int(input("@> "))
         except ValueError:
             print("Invalid index")
+            # print("Niepoprawna wartość")
         else:
             purchased_products.pop(index)
+
     elif option == "3":
         print("Confirm")
         if input("1> ") == "1":
             purchased_products = []
         else:
             print("Cancelled")
+
     elif option == "4":
         now = datetime.now()
         receipt = generate_receipt(purchased_products, now.strftime("%d.%m.%Y %H:%M"))
         print(receipt)
         print("Confirm")
         if input("1> ") == "1":
-            print_receipt(receipt, now.strftime("%d.%m.%Y %H:%M:%S"))
-            print("Printing...")
-            save_logs(purchased_products, now.strftime("%d.%m.%Y %H:%M:%S"))
+            try:
+                print_receipt_network_printer(receipt)
+            except:
+                print("Printing error")
+                # print("Błąd drukowania")
+            else:
+                print("Printing...")
+                # print("Drukowanie...")
+            filename = now.strftime("%d_%m_%Y__%H_%M_%S")
+            save_receipt_as_txt(receipt, filename + ".txt")
+            save_logs(purchased_products, filename + ".csv")
             purchased_products = []
         else:
             print("Cancelled")
+            # print("Anulowano")
+
+    elif option == "5":
+        try:
+            dir = f"{getcwd()}/receipts/"
+            files = listdir(dir)
+        except FileNotFoundError:
+            print(f"Directory doesn't exist: {dir}")
+            # print(f"Folder nie istnieje: {dir}")
+        else:
+            for index, file in enumerate(files):
+                print(f"{index}. {file}")
+            try:
+                index = int(input("@>"))
+            except ValueError:
+                print("Invalid value")
+                # print("Niepoprawna wartość")
+            else:
+                receipt = open(dir + files[index]).read()
+                print(receipt)
+                print_receipt_network_printer(receipt)       
+
     else:
         print("Invalid option")
+        # print("Niepoprawna opcja")
